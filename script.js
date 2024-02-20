1308be50e75438256cafc3026fada615f79e3689
khoutoub <117872523+khoutoub@users.noreply.github.com>
2024-02-20 15:31:21 -0500

    commit
    commit changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to fully load before running the script
+document.addEventListener('DOMContentLoaded', function () {
+    // Show an alert dialog when the page has loaded
+    alert('Today only - 10% off on a weekend - coupon code ZenTen');
+});
